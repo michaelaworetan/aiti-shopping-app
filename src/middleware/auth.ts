d01c69ae74110609c0aeb9eeed0af84ad57af240
Aworetan Michael <michaelaworetan@gmail.com>
2024-11-09 01:42:16 +0100

    added .env file

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 import { NextFunction, Request, Response } from "express"
 
-const tokenSecret = "letmein"
+const tokenSecret = process.env.TOKEN_SECRET!
 const tokenExpiration = "1 day"
 
 // Generate a token after user has signed in 

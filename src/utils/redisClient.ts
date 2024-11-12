5d113fe9d2a659c03c75b4e6723e0c55b0ef7dc2
Aworetan Michael <michaelaworetan@gmail.com>
2024-11-12 19:57:16 +0100

    added redis to cache2

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 // const redisClient = createClient({url: "redis://127.0.0.1:6379"}) // "redis://name(:)pass@redis-url"
 
-const redisClient = createClient({url: process.env.REDIS_URL!})
+const redisClient = createClient({url: process.env.REDIS_URL})
 
 redisClient.on("error", (err) => {
     console.error("Redis Client Error", err);   
